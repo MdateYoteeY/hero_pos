66669d4d03cbcafcf881e04ee9b1635400a5a4c2
MdateYotee <sumatyotee@gmail.com>
2021-03-29 16:19:49 +0700

    as

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/website_pos'));
-
+// app.use(express.static('./dist/website_pos'));
+// app.use(express.static('../dist/website_pos/'));
+app.use(express.static('../dist/website_pos/'));
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/website_pos/'}),
 );
